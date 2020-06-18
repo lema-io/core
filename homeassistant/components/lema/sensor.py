dd8438e529922686c0140aecc67e5f87000b4532
Andy Gikling <asgikling@gmail.com>
2020-06-17 23:17:53 -0500

    lema - Added switch.py.  Io is working over coap for 4 endpoints.  Switches are working.

--- homeassistant/components/lema/sensor.py
+++ homeassistant/components/lema/sensor.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 import logging
 import asyncio
 
+# Bring in CoAP
 from aiocoap import *
 
 import voluptuous as vol
@@ -10,30 +11,27 @@ import voluptuous as vol
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (
     CONF_HOST,
-    CONF_PASSWORD,
-    CONF_PATH,
+    CONF_FRIENDLY_NAME,
     CONF_SCAN_INTERVAL,
-    CONF_SSL,
-    CONF_VERIFY_SSL,
+    POWER_WATT,
+    ENERGY_KILO_WATT_HOUR,
     EVENT_HOMEASSISTANT_STOP,
 )
 from homeassistant.core import callback
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity import ToggleEntity
 from homeassistant.helpers.event import async_track_time_interval
 from homeassistant.const import ATTR_NAME, DEVICE_DEFAULT_NAME
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_CUSTOM = "custom"
-CONF_FACTOR = "factor"
-CONF_GROUP = "group"
-CONF_KEY = "key"
-CONF_SENSORS = "sensors"
-CONF_UNIT = "unit"
+CONST_DEFAULT_SCAN_PERIOD_S = 60.0
 
-GROUPS = ["user", "installer"]
+CONST_COAP_PROTOCOL = "coap://"
+CONST_COAP_STRING_TRUE = "1"
+CONST_COAP_STRING_FALSE = "1"
 
 protocol = ""
 
@@ -41,38 +39,17 @@ PLATFORM_SCHEMA = vol.All(
     PLATFORM_SCHEMA.extend(
         {
             vol.Required(CONF_HOST): cv.string,
-            # vol.Optional(CONF_SSL, default=False): cv.boolean,
-            # vol.Optional(CONF_VERIFY_SSL, default=True): cv.boolean,
-            # vol.Required(CONF_PASSWORD): cv.string
-            # vol.Optional(CONF_GROUP, default=GROUPS[0]): vol.In(GROUPS),
-            # vol.Optional(CONF_SENSORS, default=[]): vol.Any(
-            #    cv.schema_with_slug_keys(cv.ensure_list),  # will be deprecated
-            #    vol.All(cv.ensure_list, [str]),
-            # ),
-            # vol.Optional(CONF_CUSTOM, default={}): cv.schema_with_slug_keys(
-            #    CUSTOM_SCHEMA
-            # ),
+            vol.Required(CONF_FRIENDLY_NAME): cv.string,
+            vol.Optional(CONF_SCAN_INTERVAL): cv.string
         },
         extra=vol.PREVENT_EXTRA,
     )
 )
 
-
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up LEMA Off-Grid Unit."""
 
     host = config.get(CONF_HOST)
-
-    # Init all default sensors
-    # sensor_def = pysma.Sensors()
-
-    # Sensor from the custom config
-    # sensor_def.add(
-    #    [
-    #        pysma.Sensor(o[CONF_KEY], n, o[CONF_UNIT], o[CONF_FACTOR], o.get(CONF_PATH))
-    #        for n, o in config[CONF_CUSTOM].items()
-    #    ]
-    # )
 
     # Use all sensors by default
     hass_sensors = []
@@ -80,44 +57,40 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     # Setup Async CoAP
     protocol = await Context.create_client_context()
 
-    hass_sensors.append(LEMAOffGrid(host, "led/blue", protocol))
+    # Add sensors
+    hass_sensors.append(LEMAOffGridSensor(host, "led/blue", protocol, config.get(CONF_FRIENDLY_NAME) + " - Blue Led", None))
+    hass_sensors.append(LEMAOffGridSensor(host, "inv/w", protocol, config.get(CONF_FRIENDLY_NAME) + " - Inverter Out Watts", POWER_WATT))
 
     async_add_entities(hass_sensors)
 
-    # Init the SMA interface
-
-    backoff = 0
-    backoff_step = 0
-
-    async def async_lema(event):
+    async def async_update_sensors(event):
         """Update all the LEMA sensors."""
 
-        # Check for new data
+        # Update sensors based on scan_period set below which comes in from the config
         for sensor in hass_sensors:
             await sensor.async_update_values()
 
     # Call out an interval to poll data from CoAP endpoint
-    # interval = config.get(CONF_SCAN_INTERVAL) or timedelta(seconds=5)
-    interval = timedelta(seconds=0.5)
+    period_s = config.get(CONF_SCAN_INTERVAL)
+    if (period_s != None and float(period_s) > 0):
+        print("Sensor using scan period from config: " + period_s)
+        scan_period = timedelta(seconds=float(period_s))
+    else:
+        print("Sensor using default scan period of: " + str(CONST_DEFAULT_SCAN_PERIOD_S))
+        scan_period = timedelta(seconds=CONST_DEFAULT_SCAN_PERIOD_S)
 
-    print("LEMA scan interval: " + str(interval))
-
-    async_track_time_interval(hass, async_lema, interval)
+    async_track_time_interval(hass, async_update_sensors, scan_period)
 
 
-class LEMAOffGrid(Entity):
-    """Representation of a LEMA Off-Grid Solar Power Supply."""
+class LEMAOffGridSensor(Entity):
+    """Representation of a LEMA Off-Grid Solar Power Supply Sensor."""
 
-    def __init__(self, host, uri, protocol):
+    def __init__(self, host, uri, protocol, name, unit):
         """Initialize the sensor."""
         self._uri = uri
-        self._name = "lema-off-grid"
-        self._uri = ""
-        self._value = 0
-        self._unit = "no units..."
-        self._attr = {"20"}
-        # self._attr = {s.name: "" for s in sub_sensors}
-        self._state = self._value
+        self._name = name
+        self._unit = unit
+        self._state = "0.0"
         self._host = host
         self._protocol = protocol
 
@@ -137,101 +110,31 @@ class LEMAOffGrid(Entity):
         return self._unit
 
     @property
-    def device_state_attributes(self):  # Can be remove from 0.99
-        """Return the state attributes of the sensor."""
-        return self._attr
-
-    @property
     def poll(self):
-        """SMA sensors are updated & don't poll."""
-        return False
+        """LEMA sensors are polled."""
+        return True
 
     @callback
     async def async_update_values(self):
         """Update this sensor."""
-        update = False
-
-        print("LEMA async_update_values...")
-        # for sens in self._sub_sensors:  # Can be remove from 0.99
-        #    newval = f"{sens.value} {sens.unit}"
-        #    if self._attr[sens.name] != newval:
-        #        update = True
-        #        self._attr[sens.name] = newval
-
-        # if self._sensor.value != self._state:
-        #    update = True
-        #    self._state = self._sensor.value
-
-        _LOGGER.info("LEMA calling coap")
-        # if update:
-        request = Message(code=GET, uri="coap://" + self._host + "/led/blue")
-
         try:
+            request = Message(code=GET, uri=CONST_COAP_PROTOCOL + self._host + "/" + self._uri)
             response = await self._protocol.request(request).response
-        except Exception as e:
-            print("Failed to fetch resource:")
-            print(e)
-        else:
-            print("Result: %s\n%r" % (response.code, response.payload))
 
-            self._value = response.payload
-            self._state = self._value
-            self.async_write_ha_state()
+            # Check for change
+            if (self._state != float(response.payload)):
+                self._state = float(response.payload)
+                _LOGGER.info("%s changed: %s - %r" % (self._uri, response.code, response.payload))
+                self.async_write_ha_state()
+            else:
+                _LOGGER.info("%s no change... current value = %s" % (self._uri, self._state))
+
+        except Exception as e:
+            _LOGGER.info("Failed to GET resource: " + self._uri)
+            _LOGGER.info(e)
 
     @property
     def unique_id(self):
         """Return a unique identifier for this sensor."""
-        return f"leam-{self._uri}-{self._name}"
-
-
-class IOSwitch(ToggleEntity):
-    """Representation of a Digital Output."""
-
-    def __init__(self, host, uri, name, invert_logic):
-        """Initialize the pin."""
-        self._host = host
-        self._uri = uri
-        self._name = name or DEVICE_DEFAULT_NAME
-        self._invert_logic = invert_logic
-        self._state = False
-        rpi_pfio.write_output(self._uri, 1 if self._invert_logic else 0)
-
-    @property
-    def name(self):
-        """Return the name of the switch."""
-        return self._name
-
-    @property
-    def should_poll(self):
-        """Return the polling state."""
-        return False
-
-    @property
-    def is_on(self):
-        """Return true if device is on."""
-        return self._state
-
-    def turn_on(self, **kwargs):
-        """Turn the device on."""
-        rpi_pfio.write_output(self._uri, 0 if self._invert_logic else 1)
-        self._state = True
-        self.schedule_update_ha_state()
-
-    def turn_off(self, **kwargs):
-        """Turn the device off."""
-
-        request = Message(code=PUT, value="0", uri="coap://" + self._host + self._uri)
-
-        try:
-            response = await self._protocol.request(request).response
-        except Exception as e:
-            print("Failed to fetch resource:")
-            print(e)
-        else:
-            print("Result: %s\n%r" % (response.code, response.payload))
-
-
-        rpi_pfio.write_output(self._uri, 1 if self._invert_logic else 0)
-        self._state = False
-        self.schedule_update_ha_state()
+        return f"{self._name}"
 
---
+++ homeassistant/components/lema/switch.py
@@ -0,0 +1,163 @@
+"""LEMA Off-Grid interface."""
+from datetime import timedelta
+import logging
+import asyncio
+
+# Bring in CoAP
+from aiocoap import *
+
+import voluptuous as vol
+
+from homeassistant.components.switch import PLATFORM_SCHEMA
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_FRIENDLY_NAME,
+    CONF_SCAN_INTERVAL,
+    POWER_WATT,
+    ENERGY_KILO_WATT_HOUR,
+    EVENT_HOMEASSISTANT_STOP,
+)
+from homeassistant.core import callback
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity import ToggleEntity
+from homeassistant.helpers.event import async_track_time_interval
+from homeassistant.const import ATTR_NAME, DEVICE_DEFAULT_NAME
+
+_LOGGER = logging.getLogger(__name__)
+
+CONST_DEFAULT_SCAN_PERIOD_S = 60.0
+
+CONST_COAP_PROTOCOL = "coap://"
+CONST_COAP_STRING_TRUE = "1"
+CONST_COAP_STRING_FALSE = "0"
+
+protocol = ""
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_HOST): cv.string,
+        vol.Required(CONF_FRIENDLY_NAME): cv.string,
+        vol.Optional(CONF_SCAN_INTERVAL): cv.string
+    }
+)
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up LEMA Off-Grid Unit Switchs."""
+
+    host = config.get(CONF_HOST)
+
+    # Use all switches by default
+    hass_switches = []
+
+    # Setup Async CoAP
+    protocol = await Context.create_client_context()
+
+    # Add switches
+    hass_switches.append(LEMAIOSwitch(host, "io/r1", protocol, config.get(CONF_FRIENDLY_NAME) + " - Outlet 1", False, None))
+    hass_switches.append(LEMAIOSwitch(host, "io/r2", protocol, config.get(CONF_FRIENDLY_NAME) + " - Outlet 2", False, None))
+
+    async_add_entities(hass_switches)
+
+    async def async_update_switches(event):
+        """Update all the LEMA switches."""
+
+        # Update sensors based on scan_period set below which comes in from the config
+        for sw in hass_switches:
+            await sw.async_update_values()
+
+    # Call out an interval to poll data from CoAP endpoint
+    period_s = config.get(CONF_SCAN_INTERVAL)
+    if (period_s != None and float(period_s) > 0):
+        print("Switch using scan period from config: " + period_s)
+        scan_period = timedelta(seconds=float(period_s))
+    else:
+        print("Switch using default scan period of: " + str(CONST_DEFAULT_SCAN_PERIOD_S))
+        scan_period = timedelta(seconds=CONST_DEFAULT_SCAN_PERIOD_S)
+
+    async_track_time_interval(hass, async_update_switches, scan_period)
+
+
+class LEMAIOSwitch(ToggleEntity):
+    """Representation of a Digital Output."""
+
+    def __init__(self, host, uri, protocol, name, unit, invert_logic):
+        """Initialize the pin."""
+        self._host = host
+        self._uri = uri
+        self._name = name
+        self._unit = unit
+        self._invert_logic = invert_logic
+        self._state = False
+        self._protocol = protocol
+        self.async_turn_off()
+
+    @property
+    def name(self):
+        """Return the name of the switch."""
+        return self._name
+
+    @property
+    def should_poll(self):
+        """Return the polling state."""
+        return True
+
+    @property
+    def is_on(self):
+        """Return true if device is on."""
+        return self._state
+
+    async def async_turn_on(self, **kwargs):
+        """Turn the device on."""
+        try:
+            _LOGGER.info("LEMA calling TURN_ON for " + self._uri)
+            request = Message(code=PUT, payload=CONST_COAP_STRING_TRUE.encode("ascii"), uri=CONST_COAP_PROTOCOL + self._host + "/" + self._uri)
+            response = await self._protocol.request(request).response
+            self._state = True
+            self.schedule_update_ha_state()
+        except Exception as e:
+            _LOGGER.info("Failed to PUT resource: " + self._uri)
+            _LOGGER.info(e)
+
+    async def async_turn_off(self, **kwargs):
+        """Turn the device off."""
+        try:
+            _LOGGER.info("LEMA calling TURN_OFF for " + self._uri)
+            request = Message(code=PUT, payload=CONST_COAP_STRING_FALSE.encode("ascii"), uri=CONST_COAP_PROTOCOL + self._host + "/" + self._uri)
+            response = await self._protocol.request(request).response
+            self._state = False
+            self.schedule_update_ha_state()
+        except Exception as e:
+            _LOGGER.info("Failed to PUT resource: " + self._uri)
+            _LOGGER.info(e)
+
+    @callback
+    async def async_update_values(self):
+        """Update this switch."""
+        try:
+            request = Message(code=GET, uri=CONST_COAP_PROTOCOL + self._host + "/" + self._uri)
+            response = await self._protocol.request(request).response
+
+            response_bool = False
+
+            if (response.payload == b'1'):
+                response_bool = True
+
+            # Check for change
+            if (self._state != response_bool):
+                self._state = response_bool
+                _LOGGER.info("%s changed: %s - %s" % (self._uri, response.code, str(response_bool)))
+                self.async_write_ha_state()
+            else:
+                _LOGGER.info("%s no change..." % (self._uri))
+
+        except Exception as e:
+            _LOGGER.info("Failed to GET resource: " + self._uri)
+            _LOGGER.info(e)
+
+    @property
+    def unique_id(self):
+        """Return a unique identifier for this sensor."""
+        return f"{self._name}"
+
